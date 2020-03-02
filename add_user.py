b9c20b285b12e5bbb6a7d60a85375ff766990dd5
Greg <36547226+greglenard@users.noreply.github.com>
2020-03-02 16:16:39 -0700

    Update add_user.py
    asdfasdf

--- add_user.py
+++ add_user.py
@@ -6,7 +6,19 @@
     The solution contains the same number of lines (plus imports)
 """
 
-import bcrypt
+import csv
+import os
+import hashlib
+import Crypto
+import pickle
+
+try:
+    test = pickle.load(open('passfile.pkl', 'rb'))
+except:
+    dict_init = {'user': ('salt','hp')}
+    pw_dict = open("passfile.pkl","wb")
+    pickle.dump(dict_init, pw_dict)
+    pw_dict.close()
 
 user = input("Enter a username: ")
 password = input("Enter a password: ")
@@ -15,19 +27,70 @@ password = input("Enter a password: ")
 # salt = ???
 # hashed_password = ???
 
-salt = bcrypt.gensalt()
-hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+salt = Crypto.Random.get_random_bytes(2)
+hashed_password = hashlib.sha512(salt + password)
 
 try:
-    reading = open("passfile.txt", 'r')
-    for line in reading.read().split('\n'):
-        if line.split('\t')[0] == user:
-            print("User already exists!")
-            exit(1)
-    reading.close()
+    pw_file = open("passfile.pkl", 'r')
+    pw_dict = pickle.load(pw_file)
+    if user in pw_dict:
+        print("User already exists!")
+        exit(1)
+    else:
+        pw_dict[user] = (salt, hashed_password)
+        print("User successfully added!")
+        pickle.dump(pw_dict, pw_file)
+        pw_dict.close()
+"""
+    add_user.py - Stores a new username along with salt/password
+
+    CSCI 3403
+    Authors: Matt Niemiec and Abigail Fernandes
+    The solution contains the same number of lines (plus imports)
+"""
+
+import csv
+import os
+import hashlib
+import Crypto
+import pickle
+
+try:
+    test = pickle.load(open('passfile.pkl', 'rb'))
+except:
+    dict_init = {'user': ('salt','hp')}
+    pw_dict = open("passfile.pkl","wb")
+    pickle.dump(dict_init, pw_dict)
+    pw_dict.close()
+
+user = input("Enter a username: ")
+password = input("Enter a password: ")
+
+# TODO: Create a salt and hash the password
+# salt = ???
+# hashed_password = ???
+
+salt = Crypto.Random.get_random_bytes(2)
+hashed_password = hashlib.sha512(salt + password)
+
+try:
+    pw_file = open("passfile.pkl", 'r')
+    pw_dict = pickle.load(pw_file)
+    if user in pw_dict:
+        print("User already exists!")
+        exit(1)
+    else:
+        pw_dict[user] = (salt, hashed_password)
+        print("User successfully added!")
+        pickle.dump(pw_dict, pw_file)
+        pw_dict.close()
+
+
 except FileNotFoundError:
     pass
 
-with open("passfile.txt", 'a+') as writer:
-    writer.write("{0}\t{1}\t{2}\n".format(user, salt, hashed_password))
-    print("User successfully added!")
+
+
+except FileNotFoundError:
+    pass
+
