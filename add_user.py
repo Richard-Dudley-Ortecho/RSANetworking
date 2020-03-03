a4383255b467785cc790620fae6685f3bd814067
Richard-Dudley-Ortecho <dudley.o@hotmail.com>
2020-03-02 19:32:29 -0700

    Fully complete with add_user

--- add_user.py
+++ add_user.py
@@ -1,15 +1,8 @@
-"""
-
-    CSCI 3403
-    Authors: Matt Niemiec and Abigail Fernandes
-    The solution contains the same number of lines (plus imports)
-"""
-
 import bcrypt
-
 user = input("Enter a username: ")
 password = input("Enter a password: ")
-
+#print(user)
+#print(password)
 # TODO: Create a salt and hash the password
 # salt = ???
 # hashed_password = ???
@@ -17,16 +10,23 @@ password = input("Enter a password: ")
 salt = bcrypt.gensalt()
 hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
+
 try:
-    reading = open("passfile.txt", 'r')
+    reading = open("passfile.txt" , 'r')
     for line in reading.read().split('\n'):
         if line.split('\t')[0] == user:
             print("User already exists!")
             exit(1)
-    reading.close()
+        reading.close()
 except FileNotFoundError:
     pass
 
-with open("passfile.txt", 'a+') as writer:
-    writer.write("{0}\t{1}\t{2}\n".format(user, salt.decode('utf-8'), hashed_password.decode('utf-8'))
+salt_u = salt.decode('utf-8')
+hashed_password_u = hashed_password.decode('utf-8')
 
+#print(salt_u)
+#print(hashed_password_u)
+
+with open("passfile.txt", 'a+') as writer:
+    writer.write("{0}\t{1}\t{2}\n".format(user, salt_u, hashed_password_u))
+print("Added User!")
